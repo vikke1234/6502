ea65265abbee211e3e3031e8e8df621ef4f56a8f
Viktor Horsmanheimo <Viktor.horsmanheimo@teknik.io>
2019-04-22 02:06:27 +0300

    added the rest of the functions to the header file

--- headers/cpu.h
+++ headers/cpu.h
@@ -2,8 +2,7 @@
 #define CPU_H
 #include <stdint.h>
 #include <stdlib.h>
-
-extern void interpret_opcode(void);
+#include <stdbool.h>
 
 typedef enum {
   IMPLICT,
@@ -54,10 +53,9 @@ typedef struct {
   uint8_t status;          /** NVsB DIZC, @see FLAGS */
 } processor_registers;
 
+extern void interpret_opcode(void);
 extern void initialize_cpu(const unsigned char *data, size_t size, memory_map *m, processor_registers *reg);
 
-
-/* TODO: add small documentation on theese, maybe not and just link documentation */
 
 void ADC_help(uint16_t value);
 void ADC_im(void);
@@ -69,7 +67,6 @@ void ADC_absolutex(void);
 void ADC_absolutey(void);
 void ADC_indirectx(void);
 void ADC_indirecty(void);
-
 void AND_help(uint8_t value);
 void AND_im(void);
 void AND_zero(void);
@@ -79,15 +76,16 @@ void AND_absolutex(void);
 void AND_absolutey(void);
 void AND_indirectx(void);
 void AND_indirecty(void);
+void ASL_help(uint8_t *address);
 void ASL_accumulator(void);
 void ASL_zero(void);
 void ASL_zerox(void);
 void ASL_absolute(void);
 void ASL_absolutex(void);
-
 void BCC(void);
 void BCS(void);
 void BEQ(void);
+void BIT_help(uint16_t bit_test);
 void BIT_zero(void);
 void BIT_absolute(void);
 void BMI(void);
@@ -96,12 +94,12 @@ void BPL(void);
 void BRK(void);
 void BVC(void);
 void BVS(void);
-
 void CLC(void);
 void CLD(void);
 void CLI(void);
 void CLV(void);
-
+void cmp_help(unsigned char value, unsigned char reg);
+void CMP_help(unsigned char value);
 void CMP_im(void);
 void CMP_zero(void);
 void CMP_zerox(void);
@@ -110,32 +108,40 @@ void CMP_absolutex(void);
 void CMP_absolutey(void);
 void CMP_indirectx(void);
 void CMP_indirecty(void);
-
 void CPX_help(unsigned char value);
 void CPX_im(void);
 void CPX_zero(void);
 void CPX_absolute(void);
-
 void CPY_help(unsigned char value);
 void CPY_im(void);
 void CPY_zero(void);
 void CPY_absolute(void);
-
-void DEC_help(void);
+void DEC_help(uint16_t location);
 void DEC_zero(void);
 void DEC_zerox(void);
 void DEC_absolute(void);
 void DEC_absolutex(void);
-
 void DEX(void);
 void DEY(void);
-void EOR(void);
-void INC(void);
+void EOR_help(uint8_t value);
+void EOR_im(void);
+void EOR_zero(void);
+void EOR_zerox(void);
+void EOR_absolute(void);
+void EOR_absolutex(void);
+void EOR_absolutey(void);
+void EOR_indirectx(void);
+void EOR_indirecty(void);
+void INC_help(uint16_t location);
+void INC_zero(void);
+void INC_zerox(void);
+void INC_absolute(void);
+void INC_absolutex(void);
 void INX(void);
 void INY(void);
-void JMP(void);
+void JMP_absolute(void);
+void JMP_indirect(void);
 void JSR(void);
-
 void LDA_help(uint16_t value);
 void LDA_im(void);
 void LDA_zero(void);
@@ -145,21 +151,18 @@ void LDA_absolutex(void);
 void LDA_absolutey(void);
 void LDA_indirectx(void);
 void LDA_indirecty(void);
-
 void LDX_help(uint8_t value);
 void LDX_im(void);
 void LDX_zero(void);
 void LDX_zeroy(void);
 void LDX_absolute(void);
 void LDX_absolutey(void);
-
 void LDY_help(uint8_t value);
-void LDY_im (void);
+void LDY_im(void);
 void LDY_zero(void);
 void LDY_zerox(void);
 void LDY_absolute(void);
 void LDY_absolutex(void);
-
 void LSR_help(uint8_t *value);
 void LSR_accumulator(void);
 void LSR_zero(void);
@@ -206,9 +209,19 @@ void SBC_indirecty(void);
 void SEC(void);
 void SED(void);
 void SEI(void);
-void STA(void);
-void STX(void);
-void STY(void);
+void STA_zero(void);
+void STA_zerox(void);
+void STA_absolute(void);
+void STA_absolutex(void);
+void STA_absolutey(void);
+void STA_indirectx(void);
+void STA_indirecty(void);
+void STX_zero(void);
+void STX_zeroy(void);
+void STX_absolute(void);
+void STY_zero(void);
+void STY_zerox(void);
+void STY_absolute(void);
 void TAX(void);
 void TAY(void);
 void TSX(void);
