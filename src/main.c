3100df3ab12715fbe07d7bb3c1331c9972a9e99d
Viktor Horsmanheimo <viktor.horsmanheimo@helsinki.fi>
2019-10-20 14:43:42 +0300

    moved code from gui.c to main.c

--- src/main.c
+++ src/main.c
@@ -1,27 +1,22 @@
 #include "../headers/cpu.h"
-#include "../headers/file_handler.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/stat.h>
-#include <unistd.h>
 
 void print_help();
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
-    puts("error no filename given");
-    return 0;
+    puts("Please specify file name");
+    exit(1);
   }
-  struct stat s;
-  stat(argv[1], &s);
-  FILE *file = fopen(argv[1], "rb");
 
-  uint8_t *data = (uint8_t *)calloc(sizeof(char), s.st_size);
-  fread(data, 1, s.st_size, file);
-  initialize_cpu(data, s.st_size);
-  for (long i = 0; i < s.st_size - 0x600; i++) {
+  if (initialize_cpu_filename(argv[1]) == false) {
+    fprintf(stderr,
+            "Error: could not initialize cpu, file could no be accessed");
+    return 1;
+  }
+
+  while(1) {
     interpret_opcode();
   }
-
   return 0;
 }
--- src/ui/gui.c
+++ src/ui/gui.c
@@ -1,40 +1,6 @@
 #include "../../headers/cpu.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <SDL2/SDL.h>
 
-#ifdef _WIN32
-#define _CRT_SECURE_NO_WARNINGS
-#endif
 
-int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    puts("Please specify file name");
-    exit(1);
-  }
-
-  if (initialize_cpu_filename(argv[1]) == false) {
-    fprintf(stderr,
-            "Error: could not initialize cpu, file could no be accessed");
-    return 1;
-  }
-
-  int input = '\0';
-  while (input != 'e' && input != 'q') {
-    input = 'n';
-    switch (input) {
-    case 'H':
-    case 'h':
-      break;
-    case 'N':
-    case 'n':
-    case '\n':
-      interpret_opcode();
-      break;
-
-    default:
-      break;
-    }
-  }
-
-  return 0;
-}
